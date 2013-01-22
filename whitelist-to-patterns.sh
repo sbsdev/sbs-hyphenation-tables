7c91c9024a1d376d63fd4ea3f02632f76d3fab3c
Bert Frees <bert.frees@sbs.ch>
2013-01-22 12:29:14 +0100

    Fix encoding problem

--- whitelist-to-patterns.sh
+++ whitelist-to-patterns.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-tr A-Z a-z | tr ' ' '\n' | grep -v '^$' |
-awk '{printf "%c", "."
-for (i=1; i <= length($1); i++){printf "%c8", substr($1, i, 1)}
-print "."}' | sed 's/8-8/9/g;s/8\.$/./;'
+LANG=de_DE.UTF-8
+recode latin1..UTF-8 \
+| tr '[:upper:]' '[:lower:]' \
+| grep -v '^$' \
+| sed 's/\(.\)/\18/g' \
+| sed 's/^\(.*\)$/\.\1\./' \
+| sed 's/8-8/9/g;s/8\.$/./;' \
+| recode UTF-8..latin1
